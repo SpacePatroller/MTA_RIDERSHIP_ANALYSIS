409d8e96b7774c706b66df94ba443c4bed950a7b
SpacePatroller <emanshupatel@Emanshus-MacBook-Pro.local>
2019-02-08 00:47:16 -0600

    working on design

--- static/js/logic.js
+++ static/js/logic.js
@@ -3,7 +3,7 @@ var mymap = L.map('mapid').setView([40.7128,-74.0060], 13);
 // tile layer
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
+    maxZoom: 11,
     id: "mapbox.streets",
     accessToken: API_KEY
 }).addTo(mymap);
