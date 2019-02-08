c7a100f927816dd9c497ffba96f22c217296ba52
SpacePatroller <emanshupatel@Emanshus-MacBook-Pro.local>
2019-02-07 20:06:43 -0600

    Laying mortor

--- app.py
+++ app.py
@@ -6,9 +6,36 @@ from flask import (
     request,
     redirect)
 
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import inspect
+
 app = Flask(__name__)
 
-# need to set up connection to a test db through sqlalchemy
+# Connection to data base
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1:3306/sakila'
+# db = SQLAlchemy(app)
+
+engine = create_engine('mysql://root:@127.0.0.1:3306/sakila')
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+session = Session(engine)
+inspector = inspect(engine)
+
+# Get table information
+print(inspector.get_table_names())
+# Get column information
+print(inspector.get_columns('city'))
+
+cities = Base.classes.city
+
 
 
 # route to main index.html template
@@ -16,6 +43,12 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+
+@app.route("/test")
+def test():
+    city = session.query(cities.city).all()
+
+    return jsonify(city)
 
 if __name__ == "__main__":
     app.run()
