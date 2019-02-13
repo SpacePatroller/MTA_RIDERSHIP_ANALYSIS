d5949291bd552c5d1c08c5353ba19a4398dcdba5
SpacePatroller <emanshupatel@Emanshus-MacBook-Pro.local>
2019-02-12 21:48:48 -0600

    icons added

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,16 +1,23 @@
-
-
-var elevated = [];
-var subway = [];
-var open_cut = [];
-var viaduct = [];
-var at_grade = [];
-var embankment = [];
+var greenIcon = L.icon({
+    iconUrl: '/static/images/bus-stop3.png',
+    iconSize:     [12, 10], // size of the icon
+    // shadowSize:   [50, 64], // size of the shadow
+    // iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    // shadowAnchor: [4, 62],  // the same for the shadow
+    // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 
 // read in latitudes and longitudes from /locations routes to plot stop markers
 locationUrl = '/locations'
 d3.json(locationUrl).then(function (locations) {
     console.log(locations)
+
+    var elevated = [];
+    var subway = [];
+    var open_cut = [];
+    var viaduct = [];
+    var at_grade = [];
+    var embankment = [];
 
     //Loop through the initial array and add to two different arrays based on the specified variable
     for (var i = 0; i < locations.length; i++) {
@@ -24,30 +31,28 @@ d3.json(locationUrl).then(function (locations) {
 
         switch (locations[i][5]) {
             case 'Elevated':
-                elevated.push(L.marker([lat, lon],{ alt: structure}).bindTooltip(`<p>Line-${line}</p><hr><p>Stop-${stopName}</p><hr><p>Divsion-${division}`));
+                elevated.push(L.marker([lat, lon], { alt: structure, icon: greenIcon }).bindTooltip(`<p>Line-${line}</p><hr><p>Stop-${stopName}</p><hr><p>Divsion-${division}`));
                 break;
             case 'Subway':
-                subway.push(L.marker([lat, lon]).bindTooltip(`<p>Line-${line}</p><hr><p>Stop-${stopName}</p><hr><p>Divsion-${division}`));
+                subway.push(L.marker([lat, lon],{ alt: structure,icon: greenIcon }).bindTooltip(`<p>Line-${line}</p><hr><p>Stop-${stopName}</p><hr><p>Divsion-${division}`));
                 break;
             case 'Open Cut':
-                open_cut.push(L.marker([lat, lon]).bindTooltip(`<p>Line-${line}</p><hr><p>Stop-${stopName}</p><hr><p>Divsion-${division}`));
+                open_cut.push(L.marker([lat, lon],{ alt: structure,icon: greenIcon }).bindTooltip(`<p>Line-${line}</p><hr><p>Stop-${stopName}</p><hr><p>Divsion-${division}`));
                 break;
             case 'Viaduct':
-                viaduct.push(L.marker([lat, lon]).bindTooltip(`<p>Line-${line}</p><hr><p>Stop-${stopName}</p><hr><p>Divsion-${division}`));
+                viaduct.push(L.marker([lat, lon],{ alt: structure ,icon: greenIcon}).bindTooltip(`<p>Line-${line}</p><hr><p>Stop-${stopName}</p><hr><p>Divsion-${division}`));
                 break;
             case 'At Grade':
-                at_grade.push(L.marker([lat, lon]).bindTooltip(`<p>Line-${line}</p><hr><p>Stop-${stopName}</p><hr><p>Divsion-${division}`));
+                at_grade.push(L.marker([lat, lon],{ alt: structure,icon: greenIcon }).bindTooltip(`<p>Line-${line}</p><hr><p>Stop-${stopName}</p><hr><p>Divsion-${division}`));
                 break;
             case 'Embankment':
-                embankment.push(L.marker([lat, lon]).bindTooltip(`<p>Line-${line}</p><hr><p>Stop-${stopName}</p><hr><p>Divsion-${division}`));
+                embankment.push(L.marker([lat, lon],{ alt: structure,icon: greenIcon }).bindTooltip(`<p>Line-${line}</p><hr><p>Stop-${stopName}</p><hr><p>Divsion-${division}`));
                 break;
             default:
                 break;
         }
 
     }
-
-   
 
     //add the groups of markers to layerGroups
     var groupA = L.layerGroup(elevated);
@@ -72,7 +77,8 @@ d3.json(locationUrl).then(function (locations) {
         layers: [tileLayer['Structures'], groupA] //change this to determine which ones start loaded on screen
     });
 
-    ///////////Control on the Top Left that handles the switching between A and B
+    
+    ///////////Control on the Top Left that handles the switching between layers
     var overlayMaps = {
         "Elevated": groupA,
         "Subway": groupB,
