710bcb5ca72604d1249917073db570ffd308b821
SpacePatroller <emanshupatel@Emanshus-MacBook-Pro.local>
2019-02-15 12:31:02 -0600

    test bar chart working kinda

--- static/js/logic.js
+++ static/js/logic.js
@@ -110,12 +110,6 @@ plotStopsOnMap()
 
 //////////////////////////// Sams Chart /////////////////////////////////////
 
-var fareType = ["FF", "30-D UNL", "7-D UNL", "SEN/DIS", "7-D AFAS UNL", "30-D AFAS/RMF UNL",
-    "JOINT RR TKT", "14-D RFM UNL", "1-D UNL", "14-D UNL", "7D-XBUS PASS", "TCMC",
-    "RF 2 TRIP", "RR UNL NO TRADE", "TCMC ANNUAL MC", "MR EZPAY EXP", "MR EZPAY UNL", "PATH 2-T",
-    "AIRTRAIN FF", "AIRTRAIN 30-D", "AIRTRAIN 10-T", "AIRTRAIN MTHLY", "STUDENTS", "NICE 2-T",
-    "CUNY-120", "CUNY-60"];
-
 // on stop click grab station id and build chart
 uniqueStopID = []
 d3.selectAll('#mapid').on("click", function () {
@@ -134,149 +128,11 @@ d3.selectAll('#mapid').on("click", function () {
         // // create dict that holds stations   
         var stationMap = {};
 
-        stationMap[faredata[0]] = faredata[1];
+        stationMap[`"${faredata[0]}"`] = faredata[1];
 
         console.log(stationMap);
 
-        // makeVis(fareData);
-        var nutritionFields = ["FF", "30-D UNL", "7-D UNL", "SEN/DIS", "7-D AFAS UNL", "30-D AFAS/RMF UNL",
-            "JOINT RR TKT", "14-D RFM UNL", "1-D UNL", "14-D UNL", "7D-XBUS PASS", "TCMC",
-            "RF 2 TRIP", "RR UNL NO TRADE", "TCMC ANNUAL MC", "MR EZPAY EXP", "MR EZPAY UNL", "PATH 2-T",
-            "AIRTRAIN FF", "AIRTRAIN 30-D", "AIRTRAIN 10-T", "AIRTRAIN MTHLY", "STUDENTS", "NICE 2-T",
-            "CUNY-120", "CUNY-60"];
-
-            makeVis(stationMap);
-    });
-
-    var makeVis = function (stationMap) {
-        // Define dimensions of vis
-        var margin = { top: 30, right: 50, bottom: 80, left: 50 },
-            width = 1000 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
-    
-        // Make x scale
-        var xScale = d3.scaleBand()
-            .domain(fareType)
-            .padding([0.1])
-            .range([0, width]);
-    
-    
-        // .rangeRound([0, width], 0.1);
-    
-        // Make y scale, the domain will be defined on bar update
-        var yScale = d3.scaleLinear()
-            .range([height, 0]);
-    
-        // Create canvas
-        var canvas = d3.select("#mtaChart")
-            .append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    
-        // Make x-axis and add to canvas
-        var xAxis = d3.axisBottom()
-            .scale(xScale)
-        //.orient("bottom");
-    
-        //canvas.append("g")
-        //  .attr("class", "x axis")
-        //  .attr("transform", "translate(0," + height + ")")
-        //  .call(xAxis);
-    
-        canvas.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis)
-            .selectAll("text")
-            .style("text-anchor", "end")
-            .attr("dx", "-.8em")
-            .attr("dy", ".15em")
-            .attr("transform", "rotate(-35)");
-    
-        // Make y-axis and add to canvas
-        var yAxis = d3.axisLeft()
-            .scale(yScale)
-        //.orient("left");
-    
-        var yAxisHandleForUpdate = canvas.append("g")
-            .attr("class", "y axis")
-            .call(yAxis);
-    
-        yAxisHandleForUpdate.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", ".71em")
-            .style("text-anchor", "end")
-            .text("Value");
-    
-        var updateBars = function (data) {
-            // First update the y-axis domain to match data
-            yScale.domain(d3.extent(data));
-            yAxisHandleForUpdate.call(yAxis);
-    
-            var bars = canvas.selectAll(".bar").data(data);
-    
-            // Add bars for new data
-            bars.enter()
-                .append("rect")
-                .attr("class", "bar")
-                .attr("x", function (d, i) { return xScale(fareType[i]); })
-                .attr('width', xScale.bandwidth())
-                .attr("y", function (d, i) { return yScale(d); })
-                .attr("height", function (d, i) { return height - yScale(d); });
-    
-            // Update old ones, already have x / width from before
-            bars
-                .transition().duration(250)
-                .attr("y", function (d, i) { return yScale(d); })
-                .attr("height", function (d, i) { return height - yScale(d); });
-    
-            // Remove old ones
-            bars.exit().remove();
-        };
-    
-        // Handler for dropdown value change
-        var dropdownChange = function () {
-            var newStation = d3.select(this).property('value'),
-                newData = stationMap[newStation];
-    
-            updateBars(newData);
-        };
-    
-        // Get names of stations for dropdown
-        var stations = Object.keys(stationMap).sort();
-    
-        var dropdown = d3.select("#mtaChart")
-            .insert("select", "svg")
-            .on("change", dropdownChange);
-    
-        dropdown.selectAll("option")
-            .data(stations)
-            .enter().append("option")
-            .attr("value", function (d) { return d; })
-            .text(function (d) {
-                return d[0].toUpperCase() + d.slice(1, d.length); // capitalize 1st letter
-            });
-    
-        var initialData = stationMap[stations[0]];
-        updateBars(initialData);
-    };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    })
 
 })
 
@@ -285,10 +141,24 @@ d3.selectAll('#mapid').on("click", function () {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 /////////////////////////////test////////////////////test/////////////////////////////////////test////////////////////test
 /////////////////////////////test////////////////////test/////////////////////////////////////test////////////////////test
 /////////////////////////////test////////////////////test/////////////////////////////////////test////////////////////test
 /////////////////////////////test////////////////////test/////////////////////////////////////test////////////////////test
+
 
 // fareDataUrl = '/locations/stopID/1';
 // console.log(fareDataUrl)
@@ -299,27 +169,207 @@ d3.selectAll('#mapid').on("click", function () {
 //     console.log(`Fares ${faredata[1]}`)
 
 
-//     //////option one from first screen shot
+//     //     //////option one from first screen shot
 
-//     // var dict = []; // create an empty array
+//     //     // var dict = []; // create an empty array
 
-//     // dict.push({
-//     //     key:   `"${faredata[0]}"`,
-//     //     value: faredata[1]
-//     // });
+//     //     // dict.push({
+//     //     //     key:   `"${faredata[0]}"`,
+//     //     //     value: faredata[1]
+//     //     // });
 
-//     // console.log(dict)
+//     //     // console.log(dict)
 
+//     //     ////// option two for dict from screen shot
+//     var cerealMap = {};
 
+//     cerealMap[`"${faredata[0]}"`] = faredata[1];
 
-
-//     ////// option two for dict from screen shot
-// // var dict = {};
-
-// // dict[faredata[0]] = faredata[1];
-
-// // console.log(dict);
-
-
+//     console.log(cerealMap);
+//     makeVis(cerealMap);
 
 // })
+
+// var makeVis = function(cerealMap) {
+//     // Define dimensions of vis
+//     var margin = { top: 30, right: 50, bottom: 80, left: 50 },
+//         width  = 1000 - margin.left - margin.right,
+//         height = 500 - margin.top  - margin.bottom;
+
+
+//          nutritionFields = ["FF", "30-D UNL", "7-D UNL", "SEN/DIS", "7-D AFAS UNL", "30-D AFAS/RMF UNL",
+//         "JOINT RR TKT", "14-D RFM UNL", "1-D UNL", "14-D UNL", "7D-XBUS PASS", "TCMC",
+//         "RF 2 TRIP", "RR UNL NO TRADE", "TCMC ANNUAL MC", "MR EZPAY EXP", "MR EZPAY UNL", "PATH 2-T",
+//         "AIRTRAIN FF", "AIRTRAIN 30-D", "AIRTRAIN 10-T", "AIRTRAIN MTHLY", "STUDENTS", "NICE 2-T",
+//         "CUNY-120", "CUNY-60"];
+//     // Make x scale
+//     var xScale = d3.scale.ordinal()
+//         .domain(nutritionFields)
+//         .rangeRoundBands([0, width], 0.1);
+
+//     // Make y scale, the domain will be defined on bar update
+//     var yScale = d3.scale.linear()
+//         .range([height, 0]);
+
+//     // Create canvas
+//     var canvas = d3.select("#vis-container")
+//       .append("svg")
+//         .attr("width",  width  + margin.left + margin.right)
+//         .attr("height", height + margin.top  + margin.bottom)
+//       .append("g")
+//         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+//     // Make x-axis and add to canvas
+//     var xAxis = d3.svg.axis()
+//         .scale(xScale)
+//         .orient("bottom");
+
+//     //canvas.append("g")
+//       //  .attr("class", "x axis")
+//       //  .attr("transform", "translate(0," + height + ")")
+//       //  .call(xAxis);
+
+//     canvas.append("g")
+//         .attr("class", "x axis")
+//         .attr("transform", "translate(0," + height + ")")
+//         .call(xAxis)
+//         .selectAll("text")                          
+//         .style("text-anchor", "end")
+//         .attr("dx", "-.8em")
+//         .attr("dy", ".15em")
+//         .attr("transform", "rotate(-35)");
+
+//     // Make y-axis and add to canvas
+//     var yAxis = d3.svg.axis()
+//         .scale(yScale)
+//         .orient("left");
+
+//     var yAxisHandleForUpdate = canvas.append("g")
+//         .attr("class", "y axis")
+//         .call(yAxis);
+
+//     yAxisHandleForUpdate.append("text")
+//         .attr("transform", "rotate(-90)")
+//         .attr("y", 6)
+//         .attr("dy", ".71em")
+//         .style("text-anchor", "end")
+//         .text("Value");
+
+//     var updateBars = function(data) {
+//         // First update the y-axis domain to match data
+//         yScale.domain( d3.extent(data) );
+//         yAxisHandleForUpdate.call(yAxis);
+
+//         var bars = canvas.selectAll(".bar").data(data);
+
+//         // Add bars for new data
+//         bars.enter()
+//           .append("rect")
+//             .attr("class", "bar")
+//             .attr("x", function(d,i) { return xScale( nutritionFields[i] ); })
+//             .attr("width", xScale.rangeBand())
+//             .attr("y", function(d,i) { return yScale(d); })
+//             .attr("height", function(d,i) { return height - yScale(d); });
+
+//         // Update old ones, already have x / width from before
+//         bars
+//             .transition().duration(250)
+//             .attr("y", function(d,i) { return yScale(d); })
+//             .attr("height", function(d,i) { return height - yScale(d); });
+
+//         // Remove old ones
+//         bars.exit().remove();
+//     };
+
+//     // Handler for dropdown value change
+//     var dropdownChange = function() {
+//         var newCereal = d3.select(this).property('value'),
+//             newData   = cerealMap[newCereal];
+
+//         updateBars(newData);
+//     };
+
+//     // Get names of cereals, for dropdown
+//     var cereals = Object.keys(cerealMap).sort();
+
+//     var dropdown = d3.select("#vis-container")
+//         .insert("select", "svg")
+//         .on("change", dropdownChange);
+
+//     dropdown.selectAll("option")
+//         .data(cereals)
+//       .enter().append("option")
+//         .attr("value", function (d) { return d; })
+//         .text(function (d) {
+//             return d[0].toUpperCase() + d.slice(1,d.length); // capitalize 1st letter
+//         });
+
+//     var initialData = cerealMap[ cereals[0] ];
+//     updateBars(initialData);
+// };
+
+
+
+///////////////////////////////////////// diffrent chart type ///////////////////
+d3.selectAll('#mapid').on("click", function () {
+fareDataUrl = `/locations/stopID/${uniqueStopID}`;;
+console.log(fareDataUrl)
+
+// read in data from route
+d3.json(fareDataUrl).then(function (faredata) {
+    // console.log(`Station ${faredata[0]}`)
+    // console.log(`Fares ${faredata[1]}`)
+    for (x = 0; x < faredata.length; x++) {
+
+        console.log(faredata[1][x])
+        fares = []
+        fares.push(faredata[1][x])
+        console.log(`Fares list ${fares}`)
+
+
+        ctx = document.getElementById("myChart").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ["FF", "30-D UNL", "7-D UNL", "SEN/DIS", "7-D AFAS UNL", "30-D AFAS/RMF UNL",
+                    "JOINT RR TKT", "14-D RFM UNL", "1-D UNL", "14-D UNL", "7D-XBUS PASS", "TCMC",
+                    "RF 2 TRIP", "RR UNL NO TRADE", "TCMC ANNUAL MC", "MR EZPAY EXP", "MR EZPAY UNL", "PATH 2-T",
+                    "AIRTRAIN FF", "AIRTRAIN 30-D", "AIRTRAIN 10-T", "AIRTRAIN MTHLY", "STUDENTS", "NICE 2-T",
+                    "CUNY-120", "CUNY-60"],
+                datasets: [{
+                    label: faredata[0],
+                    data: faredata[1][x],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    }
+
+})
+
+})
